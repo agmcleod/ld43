b28c44d7fbd74c0e3c2a37b2b88967369b5bb258
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-11-23 18:18:24 -0500

    first step on rust experiment

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use gdnative::*;
+
+/// The HelloWorld "class"
+#[derive(NativeClass)]
+#[inherit(Node)]
+pub struct HelloWorld;
+
+// __One__ `impl` block can have the `#[methods]` attribute, which will generate
+// code to automatically bind any exported methods to Godot.
+#[methods]
+impl HelloWorld {
+
+    /// The "constructor" of the class.
+    fn _init(_owner: Node) -> Self {
+        HelloWorld
+    }
+
+    // In order to make a method known to Godot, the #[export] attribute has to be used.
+    // In Godot script-classes do not actually inherit the parent class.
+    // Instead they are"attached" to the parent object, called the "owner".
+    // The owner is passed to every single exposed method.
+    #[export]
+    fn _ready(&self, _owner: Node) {
+        unsafe {
+            let node = _owner.get_node(NodePath::from_str("UI/Inventory/TabContainer/Discover"));
+            if let Some(node) = node {
+                godot_print!("{:?}", node);
+            }
+        }
+
+    }
+}
+
+// Function that registers all exposed classes to Godot
+fn init(handle: gdnative::init::InitHandle) {
+    handle.add_class::<HelloWorld>();
+}
+
+// macros that create the entry-points of the dynamic library.
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
