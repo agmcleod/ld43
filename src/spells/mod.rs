678e5626cd5d8c05fc308cd272a863311934487c
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-11-27 22:17:16 -0500

    further work on spell code

--- src/spells/mod.rs
+++ src/spells/mod.rs
@@ -7,8 +7,47 @@ pub enum SPELL_STATUS_TYPE {
   FROST,
 }
 
+impl SPELL_STATUS_TYPE {
+  fn to_str(&self) -> &str {
+    match self {
+      SPELL_STATUS_TYPE::ARCANE => "arcane",
+      SPELL_STATUS_TYPE::FIRE => "fire",
+      SPELL_STATUS_TYPE::FROST => "frost",
+    }
+  }
+
+  fn from_str(value: &str) {
+    match value {
+      "arcane" => SPELL_TYPE::ARCANE,
+      "fire" => SPELL_TYPE::FIRE,
+      "frost" => SPELL_TYPE::FROST,
+      _ => panic!("Invalid type {:?}", value)
+    }
+  }
+}
+
+
 pub enum SPELL_TYPE {
   BALL,
   WAVE,
   SHEILD,
+}
+
+impl SPELL_TYPE {
+  fn to_str(&self) -> &str {
+    match self {
+      SPELL_TYPE::BALL => "ball",
+      SPELL_TYPE::WAVE => "wave",
+      SPELL_TYPE::SHIELD => "shield",
+    }
+  }
+
+  fn from_str(value: &str) {
+    match value {
+      "ball" => SPELL_TYPE::BALL,
+      "wave" => SPELL_TYPE::WAVE,
+      "shield" => SPELL_TYPE::SHEILD,
+      _ => panic!("Invalid type {:?}", value),
+    }
+  }
 }
--- src/spells/spell.rs
+++ src/spells/spell.rs
@@ -1,8 +1,37 @@
-use gdnative::{methods, Area2D, GodotString, NativeClass};
+use gdnative::{methods, Area2D, GodotString, NativeClass, Object, VariantArray, init::{ClassBuilder, Property, PropertyHint, PropertyUsage}, user_data::MutexData};
 
-#[derive(NativeClass)]
-#[inherit(Area2D)]
-pub struct Spell;
+use spells::{SPELL_TYPE, SPELL_STATUS_TYPE};
+
+pub struct Spell {
+    spell_type: SPELL_TYPE,
+    spell_status_type: SPELL_TYPE
+}
+
+impl NativeClass for Spell {
+    type Base = Area2D;
+    type UserData = MutexData<Spell>;
+
+    fn class_name() -> &'static str {
+        "Spell"
+    }
+
+    fn init(_owner: Self::Base) -> Self {
+        Self::init()
+    }
+
+    fn register_properties(builder: &ClassBuilder<Self>) {
+        builder.add_property(Property {
+            name: "Spell Type",
+            default: SPELL_TYPE::BALL.to_str(),
+            hint: PropertyHint::Enum {
+                values: &[SPELL_TYPE::BALL.to_str(), SPELL_TYPE::WAVE.to_str(), SPELL_TYPE::SHEILD.to_str()]
+            },
+            getter: |this: &Spell| this.spell_type,
+            setter: |this: &mut Spell, v| this.spell_type = SPELL_TYPE::from_str(v),
+            usage: PropertyUsage::DEFAULT
+        })
+    }
+}
 
 #[methods]
 impl Spell {
@@ -13,10 +42,13 @@ impl Spell {
     #[export]
     fn _ready(&self, mut owner: Area2D) {
         unsafe {
-            owner.connect(GodotString::from_str("body_entered"), owner, , "_onbody_entered");
+            owner.connect(GodotString::from_str("body_entered"), owner, , "_onbody_entered", VariantArray::new(), 0);
+            if ()
         }
     }
 
     #[export]
-    fn _onbody_entered() {}
+    fn _onbody_entered(body: Object) {
+        println!
+    }
 }
