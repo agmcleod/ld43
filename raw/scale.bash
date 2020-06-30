926016e3f66ec718abb945bcce7b91b9fefc0fff
Aaron McLeod <aaron.g.mcleod@gmail.com>
2020-06-29 22:29:42 -0400

    arcane blast

---
+++ raw/scale.bash
@@ -0,0 +1,4 @@
+#/bin/bash
+
+/c/Users/Aaron/Programs/Aseprite/Aseprite.exe -b $1 --scale 2 --save-as $2
+
--- spells/BlastAnimation.gd
+++ spells/BlastAnimation.gd
@@ -4,6 +4,7 @@ class_name BlastAnimation
 
 var frame_time: float = 0.0
 export var total_frame_count: int = 0
+export var frame_speed: float = 0.0
 
 func _ready():
   pass
@@ -11,7 +12,7 @@ func _ready():
 
 func _process(delta: float):
   frame_time += delta
-  if frame_time >= 0.017:
+  if frame_time >= frame_speed:
     frame_time = 0.0
     frame += 1
     if frame >= total_frame_count - 1:
