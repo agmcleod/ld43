accceeeac5630065dc1653b1c74c95cc453c03bc
Aaron McLeod <aaron.g.mcleod@gmail.com>
2021-09-15 23:03:49 -0400

    setting up native code to grab nav data

--- pathfinding-lib/src/lib.rs
+++ pathfinding-lib/src/lib.rs
@@ -1,4 +1,4 @@
-use gdnative::prelude::*;
+use gdnative::{api::TileMap, prelude::*};
 
 #[derive(NativeClass)]
 #[inherit(Node)]
@@ -11,8 +11,29 @@ impl PathFinding {
     }
 
     #[export]
-    fn _ready(&self, _owner: &Node) {
-        godot_print!("hello, world.")
+    fn _ready(&self, owner: &Node) {
+        unsafe {
+            let tile_map = owner
+                .get_node("/root/game/Navigation2D/TileMap")
+                .expect("TileMap node should exist")
+                .assume_safe()
+                .cast::<TileMap>()
+                .expect("TileMap casts to TileMap type");
+
+            let tileset = tile_map.tileset();
+            let tileset = tileset
+                .as_ref()
+                .expect("TileMap should have a tile set")
+                .assume_safe();
+
+            for id in tileset.get_tiles_ids().iter() {
+                let poly = tileset.tile_get_navigation_polygon(id.to_i64());
+                if let Some(poly) = poly {
+                    let polygon = poly.assume_safe();
+                    godot_dbg!(polygon.vertices());
+                }
+            }
+        }
     }
 }
 
