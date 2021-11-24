0560ba99ea7f9994c89adb20bd1bc885fe1d03af
Aaron McLeod <aaron.g.mcleod@gmail.com>
2021-11-24 16:18:56 -0500

    use center point of quad

--- pathfinding-lib/src/lib.rs
+++ pathfinding-lib/src/lib.rs
@@ -1,13 +1,7 @@
 use std::collections::HashMap;
-use std::cmp::Ordering;
+use std::cmp;
 
 use gdnative::{api::TileMap, prelude::*};
-use lyon_tessellation::math::{point, Point};
-use lyon_tessellation::path::Path;
-use lyon_tessellation::Orientation;
-use lyon_tessellation::{
-    geometry_builder::simple_builder, FillOptions, FillTessellator, VertexBuffers,
-};
 use nav;
 
 mod point_math;
@@ -25,39 +19,23 @@ impl PathFinding {
         PathFinding { mesh: None }
     }
 
-    fn create_mesh(&mut self, tessellate_output: &VertexBuffers<Point, u16>) {
+    fn create_mesh(&mut self, polydata: Vec<nav::PolyData>) {
         let params = nav::MeshParams {
             agent_height: None,
             agent_max_climb: None,
             agent_radius: 1.0,
             max_polys_per_region: 20,
-            mesh_origin: nav::Point::new(0.0, 0.0, 0.0),
+            mesh_origin: nav::Point::new(-32.0, 0.0, -32.0),
             voxel_length: 1.0,
-            voxels_per_region: nav::Count3D::new(10, 10, 10),
+            voxels_per_region: nav::Count3D::new(2500, 1, 2500),
         };
 
         let mut mesh =
             nav::NavMesh::new(&params).expect("Navigation mesh should be creatable from params");
 
-        let polygons = tessellate_output
-            .indices
-            .chunks(3)
-            .map(|chunk| {
-                let mut points = chunk
-                    .iter()
-                    .map(|idx| {
-                        let vert = tessellate_output.vertices.get(*idx as usize).unwrap();
-                        nav::Point::new(vert.x, 0.0, vert.y)
-                    })
-                    .collect::<Vec<nav::Point>>();
-                points.sort_by(point_math::sort);
-                nav::PolyData::new(points).unwrap()
-            })
-            .collect();
-
         let region = nav::Region {
             index_in_mesh: nav::Index3D::new(0, 0, 0),
-            polygons,
+            polygons: polydata,
         };
 
         mesh.put_region(&region).unwrap();
@@ -90,58 +68,57 @@ impl PathFinding {
                 }
             }
 
-            let mut tessellate_output: VertexBuffers<Point, u16> = VertexBuffers::new();
-            let mut geometry_builder = simple_builder(&mut tessellate_output);
-            let mut tessellator = FillTessellator::new();
-
-            let mut path_builder = Path::builder();
-
             let cells = tile_map.get_used_cells();
             let cells_to_map = cells.iter().filter(|vec| {
                 let vec = vec.to_vector2();
                 let id = tile_map.get_cellv(vec);
                 tile_ids_with_polygon.get(&id).is_some()
             });
-            let mut has_pathed = false;
-            let mut written_coords = Vec::new();
+
+            let mut nav_polygons = Vec::new();
+
             for vec in cells_to_map {
-                // if last loop had pathed, close it now
-                if has_pathed {
-                    path_builder.end(false);
-                }
-                has_pathed = false;
                 let vec = vec.to_vector2();
                 let id = tile_map.get_cellv(vec);
                 if let Some(polygon) = tile_ids_with_polygon.get(&id) {
                     let mut poly_coords = Vec::new();
+
+                    let mut min_x = 0.0;
+                    let mut max_x = 0.0;
+                    let mut min_z = 0.0;
+                    let mut max_z = 0.0;
+
                     for i in 0..polygon.len() {
                         let poly_vec = polygon.get(i);
-                        if i == 0 {
-                            path_builder.begin(point(poly_vec.x + vec.x, poly_vec.y + vec.y));
-                        } else {
-                            path_builder.line_to(point(poly_vec.x + vec.x, poly_vec.y + vec.y));
+                        let x = poly_vec.x + vec.x;
+                        let z = poly_vec.y + vec.y;
+                        let point = nav::Point::new(x, 0.0, z);
+
+                        if x < min_x {
+                            min_x = x;
                         }
-                        poly_coords.push((poly_vec.x + vec.x, poly_vec.y + vec.y));
-                        has_pathed = true;
+                        if x > max_x {
+                            max_x = x;
+                        }
+                        if z < min_z {
+                            min_z = z;
+                        }
+                        if z > max_z {
+                            max_z = z;
+                        }
+
+                        poly_coords.push(point);
                     }
-                    written_coords.push(poly_coords);
+
+                    let center = nav::Point::new((max_x - min_x) / 2.0 + min_x, 0.0, (max_z - min_z) / 2.0 + min_z);
+
+                    poly_coords.sort_by(point_math::sort(center));
+
+                    nav_polygons.push(nav::PolyData::new(poly_coords).unwrap());
                 }
             }
 
-            // godot_dbg!(written_coords);
-
-            path_builder.close();
-
-            let path = path_builder.build();
-
-            let result = tessellator.tessellate_path(
-                &path,
-                &FillOptions::default().with_sweep_orientation(Orientation::Horizontal),
-                &mut geometry_builder,
-            );
-            assert!(result.is_ok());
-
-            self.create_mesh(&tessellate_output);
+            self.create_mesh(nav_polygons);
         }
     }
 
--- pathfinding-lib/src/point_math.rs
+++ pathfinding-lib/src/point_math.rs
@@ -2,36 +2,13 @@ use std::cmp::Ordering;
 
 use nav;
 
-pub fn get_cross_product(a: &nav::Point, b: &nav::Point) -> f32 {
-    a.x * b.z + a.z * b.x
-}
+pub fn sort(center: nav::Point) -> impl Fn(&nav::Point, &nav::Point) -> Ordering {
+    move |a: &nav::Point, b: &nav::Point| {
+        let a_angle = (a.z - center.z).atan2(a.x - center.x);
+        let b_angle = (b.z - center.z).atan2(b.x - center.x);
 
-pub fn get_dot_product(a: &nav::Point, b: &nav::Point) -> f32 {
-    a.x * b.x - a.z * b.z
-}
-
-pub fn left_of_origin(p: &nav::Point) -> bool {
-    let u = nav::Point::new(0.0, 0.0, 1.0);
-
-    get_cross_product(&u, p) > 0.0 || (get_cross_product(&u, p) == 0.0 && get_dot_product(&u, p) > 0.0)
-}
-
-pub fn sort(a: &nav::Point, b: &nav::Point) -> Ordering {
-    // let less_than = (
-    //     left_of_origin(a) == left_of_origin(b) && get_cross_product(a, b) > 0.0
-    // ) || (left_of_origin(a) && !left_of_origin(b));
-
-    // if less_than {
-    //     return Ordering::Less
-    // }
-    // return Ordering::Greater
-
-    let origin = nav::Point::new(0.0, 0.0, 0.0);
-
-    let a_angle = (a.z - origin.z).atan2(a.x - origin.x);
-    let b_angle = (b.z - origin.z).atan2(b.x - origin.x);
-
-    a_angle.partial_cmp(&b_angle).unwrap()
+        b_angle.partial_cmp(&a_angle).unwrap()
+    }
 }
 
 #[cfg(test)]
@@ -43,16 +20,18 @@ mod test {
     #[test]
     fn test_sorts_counter_clockwise() {
         let mut vec = Vec::new();
-        vec.push(nav::Point::new(-10.0, 0.0, 10.0));
+        vec.push(nav::Point::new(0.0, 0.0, 32.0));
         vec.push(nav::Point::new(0.0, 0.0, 0.0));
-        vec.push(nav::Point::new(-10.0, 0.0, 0.0));
+        vec.push(nav::Point::new(32.0, 0.0, 0.0));
+        vec.push(nav::Point::new(32.0, 0.0, 32.0));
 
-        vec.sort_by(sort);
+        vec.sort_by(sort(nav::Point::new(16.0, 0.0, 16.0)));
 
         assert_eq!(vec, vec![
+            nav::Point::new(0.0, 0.0, 32.0),
+            nav::Point::new(32.0, 0.0, 32.0),
+            nav::Point::new(32.0, 0.0, 0.0),
             nav::Point::new(0.0, 0.0, 0.0),
-            nav::Point::new(-10.0, 0.0, 10.0),
-            nav::Point::new(-10.0, 0.0, 0.0),
         ]);
     }
 }
